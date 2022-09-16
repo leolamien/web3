bc8820534a08c47c0818ab1ffceec06e3ccbcfe5
leolamien <sienou.lamien@student.vinci.be>
2022-09-16 13:02:04 +0200

    Add ex 1.1 and 1.2

---
+++ exercices/module1/ex1.2/src/App.js
@@ -0,0 +1,45 @@
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  const Header = () => {
+    return <h1>{course}</h1>;
+  };
+  const Part = (probs) => {
+    return (
+      <div>
+        <p>
+          {probs.part} {probs.exercises}
+        </p>
+      </div>
+    );
+  };
+  const Content = () => {
+    return (
+      <div>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
+      </div>
+    );
+  };
+
+  const Total = () => {
+    return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+  };
+
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  );
+};
+
+export default App;
