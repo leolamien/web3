5e41b4934a58121c840813f23761b7a73fee66e3
leolamien <sienou.lamien@student.vinci.be>
2022-09-20 19:48:20 +0200

    module2 : events

--- exercices/module2/src/components/Button/Button.jsx
+++ exercices/module2/src/components/Button/Button.jsx
@@ -1,6 +1,30 @@
-const Button = ({ onClick, text }) => (
-    <button onClick={onClick}>
+// const Button = (text, changeCount) => {
+//   const handleClick = (e) => {
+//     let delta = e.target.dataset.delta;
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={changeCount}>{text}</button>
+//     </div>
+//   );
+// };
+
+// const Button = (props) => {
+
+//   return <button onClick={props.onClick}>{props.text}</button>;
+// };
+const Button = ({ changeCount, text, delta }) => {
+  const handleClick = (e) => {
+    let delta = e.target.dataset.delta;
+    delta = parseInt(delta);
+    changeCount(delta);
+  };
+  return (
+    <button data-delta={delta} onClick={handleClick}>
       {text}
     </button>
-  )
-  export default Button;
+  );
+};
+
+export default Button;
